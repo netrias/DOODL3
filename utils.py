98427a30e2bde4322226aed9f56d765fb065d525
Mohammed Ali Eslami <meslami@netrias.com>
2021-02-10 18:03:48 -0500

    updated utils to take epochs to save as a parameter. also made parameters to save configurable.

--- utils.py
+++ utils.py
@@ -4,23 +4,25 @@ import pandas as pd
 import numpy as np
 
 class CustomSaver(keras.callbacks.Callback):
-    def __init__(self,outdir):
+    def __init__(self,outdir,epochs=None):
         if not os.path.exists(outdir):
             os.makedirs(outdir)
-        print('set outdir',outdir)
         self.out_dir = outdir
+        if isinstance(epochs,list):
+            self.epochs = [1]+epochs
+        else:
+            self.epochs = list(range(epochs))
+
 
     def on_epoch_end(self, epoch,logs=None):
-        if epoch == 1 or epoch==11:  # or save after some epoch, each k-th epoch etc.
-            print("Saving",self.model.name,'epoch',epoch)
-            print('Saving')
-            print(os.path.join(self.out_dir,
+        if epoch in self.epochs:  # or save after some epoch, each k-th epoch etc.
+            print("Saving",self.model.name,'epoch',epoch,os.path.join(self.out_dir,
                             self.model.name + '-{}-{:1.2f}.hdf5'.format(epoch,logs['val_loss'])))
             self.model.save(os.path.join(self.out_dir,
                             self.model.name + '-{}-{:1.2f}.hdf5'.format(epoch,logs['val_loss'])))
 
 
-def train_model_iteratively(baseline_model,X_train,Y_train,X_test,Y_test,outdir,batch_size=128):
+def train_model_iteratively(baseline_model,X_train,Y_train,X_test,Y_test,outdir,epochs=12,epochs_to_save=None,batch_size=128):
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     Y_preds = []
@@ -29,11 +31,12 @@ def train_model_iteratively(baseline_model,X_train,Y_train,X_test,Y_test,outdir,
     pred_col_names = ['p'+str(i) for i in range(Y_train.shape[1])]
     for i in range(20):
         name = 'model.' + str(i)
-        print(name)
         model = baseline_model(name)
-        # saver = CustomSaver(outdir)
-        model.fit(X_train, Y_train, batch_size=batch_size, epochs=12, verbose=0,
-                                     validation_data=(X_test, Y_test) )#,callbacks=[saver]
+        if epochs_to_save is None:
+            epochs_to_save = [epochs-1]
+        saver = CustomSaver(outdir,epochs_to_save)
+        model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, verbose=0,
+                                     validation_data=(X_test, Y_test),callbacks=[saver])
         Y_pred = pd.DataFrame(model.predict(X_test), columns=[pred_col_names])
         Y_pred['class'] = Y_pred.idxmax(axis=1)
         Y_pred['model'] = name
@@ -41,11 +44,6 @@ def train_model_iteratively(baseline_model,X_train,Y_train,X_test,Y_test,outdir,
     print('appending dataframe')
     Y_predsDF = pd.concat(Y_preds)
     Y_predsDF.to_csv(os.path.join(outdir,'predictions.csv'))
-
-def get_bias(name):
-    if 'bias' in name:
-        print(name)
-        return name
 
 
 
@@ -89,7 +87,14 @@ def get_model_weights(path_to_model_files='/Users/meslami/Documents/GitRepos/dee
                     idx_dense = idx_dense+1
     return model_weights
 
-def convert_weight_dict_to_dataframe(model_weights):
+def convert_weight_dict_to_dataframe(model_weights,names_of_interest=['kernel']):
+    '''
+
+    :param model_weights: dict, model weights extracted from get_model_weights method
+    :param names_of_interest: a list of names of interest. this can be a specific layer(s). if you want all kernel parameters, then just set to 'kernel
+    otherwise, supply a list of layers ex. conv_0_kernel for all kernels of layer of first conv layer, or conv for kernels across all conv layers.
+    :return: dataframe of file as index, model_id, epoch, val_loss and all parameters indexed
+    '''
     protected_cols = ['model_id','epoch','val_loss']
     df_weights = pd.DataFrame.from_dict(model_weights).T
 
@@ -102,10 +107,12 @@ def convert_weight_dict_to_dataframe(model_weights):
         return pd.DataFrame(df[col_name].tolist(), index=df.index,
                             columns=[col_name + '_' + str(i) for i in range(len(df[col_name].iloc[0]))])
     df_kernels = [df_weights]
-    for col_name in  df_weights:
-        if 'kernel' in col_name:
-            df_kernel = explode_to_cols(df_weights, col_name)
-            df_kernels.append(df_kernel)
+    for name_of_interest in names_of_interest:
+        for col_name in  df_weights:
+            if name_of_interest in col_name:
+                df_kernel = explode_to_cols(df_weights, col_name)
+                df_kernels.append(df_kernel)
+
     df_tot = pd.concat(df_kernels, axis=1, join='inner')
     names_to_drop = [col for col in df_weights.columns if col not in protected_cols]
     df_tot.drop(names_to_drop,axis=1,inplace=True)
